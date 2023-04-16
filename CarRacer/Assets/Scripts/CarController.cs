76d66a0117e9418122661258cb34fe616ab699c1
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-04-16 12:11:46 -0700

    Create driving controller

---
+++ CarRacer/Assets/Scripts/CarController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel, false);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel, true);
+        }
+    }
+
+    public void ApplyLocalPositionToVisuals(
+        WheelCollider collider, bool isRightWheel)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        collider.GetWorldPose(
+            out Vector3 position, out Quaternion rotation);
+
+        visualWheel.transform.position = position;
+
+        if (isRightWheel)
+        {
+            rotation *= Quaternion.Euler(0, 0, 180f);
+        }
+        visualWheel.transform.rotation = rotation;
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
