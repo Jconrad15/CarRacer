10dbb56fed2310993f5c854e19c95150cdf927e2
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-04-27 10:21:18 -0700

    Start countdown

--- CarRacer/Assets/Scripts/RaceStarter.cs
+++ CarRacer/Assets/Scripts/RaceStarter.cs
@@ -1,10 +1,22 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class RaceStarter : MonoBehaviour
 {
     private Action cbOnRaceStart;
+
+    [SerializeField]
+    private TextMeshProUGUI countDownText;
+
+    [SerializeField]
+    private AudioSource source;
+
+    [SerializeField]
+    private AudioClip beepSound;
+    [SerializeField]
+    private AudioClip goSound;
 
     private void Start()
     {
@@ -13,11 +25,43 @@ public class RaceStarter : MonoBehaviour
 
     private IEnumerator StartRace()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(0.5f);
+        countDownText.gameObject.SetActive(true);
+        ShowText("3");
+        PlaySound(false);
 
+        yield return new WaitForSeconds(1);
+        ShowText("2");
+        PlaySound(false);
 
+        yield return new WaitForSeconds(1);
+        ShowText("1");
+        PlaySound(false);
+
+        yield return new WaitForSeconds(1);
+        ShowText("GO");
+        PlaySound(true);
 
         cbOnRaceStart?.Invoke();
+        yield return new WaitForSeconds(1);
+        Destroy(countDownText.gameObject);
+    }
+
+    private void ShowText(string text)
+    {
+        countDownText.SetText(text);
+    }
+
+    private void PlaySound(bool isGO)
+    {
+        if(isGO)
+        {
+            source.PlayOneShot(goSound);
+        }
+        else
+        {
+            source.PlayOneShot(beepSound);
+        }
     }
 
     public void RegisterOnRaceStart(Action callbackfunc)
